790e6d382049aec6e7d55ce26d93ea94b64b56b4
Michal Krzyzanowski <mich.krzyzanowski@gmail.com>
2017-12-29 20:37:31 +0100

    bobcat and restAssured: made arguments optional

--- vars/bobcat.groovy
+++ vars/bobcat.groovy
@@ -1,8 +1,8 @@
 try {
     withMaven(maven: 'Maven 3') {
-        sh "mvn clean test -Dmaven.test.failure.ignore=true ${config.params ?: ''}"
+        sh "mvn clean test -Dmaven.test.failure.ignore=true ${config?.params ?: ''}"
     }
 } finally {
-    junit testResults: "${config.artifactsPath ?: ''}/target/*.xml", allowEmptyResults: true
-    archiveArtifacts "${config.artifactsPath ?: ''}/target/**"
+    junit testResults: "${config?.artifactsPath ?: ''}/target/*.xml", allowEmptyResults: true
+    archiveArtifacts "${config?.artifactsPath ?: ''}/target/**"
 }
--- vars/restAssured.groovy
+++ vars/restAssured.groovy
@@ -1,8 +1,8 @@
 def call(Map config) {
     try {
-        sh "./gradlew test ${config.params ?: ''}"
+        sh "./gradlew test ${config?.params ?: ''}"
     } finally {
-        junit testResults: "${config.artifactsPath ?: ''}/build/*.xml", allowEmptyResults: true
-        archiveArtifacts "${config.artifactsPath ?: ''}/build/**"
+        junit testResults: "${config?.artifactsPath ?: ''}/build/*.xml", allowEmptyResults: true
+        archiveArtifacts "${config?.artifactsPath ?: ''}/build/**"
     }
 }
